15920bbd168a770017aaa769a79e340976477ac0
lakshmi seshu kalvakuri <42748738+seshukalvakuri@users.noreply.github.com>
2020-08-13 23:39:32 -0600

    cards

--- src/firstjavacheck/LinkedList.java
+++ src/firstjavacheck/LinkedList.java
@@ -1,6 +1,36 @@
 package firstjavacheck;
 
-public class LinkedList {
+public class LinkedList { 
+		  
+	    static Node head; 
+	  
+	    static class Node { 
+	  
+	        int data; 
+	        Node next; 
+	  
+	        Node(int d) 
+	        { 
+	            data = d; 
+	            next = null; 
+	        } 
+	    } 
+	  
+	    /* Function to reverse the linked list */
+	    Node reverse(Node node) 
+	    { 
+	        Node prev = null; 
+	        Node current = node; 
+	        Node next = null; 
+	        while (current != null) { 
+	            next = current.next; 
+	            current.next = prev; 
+	            prev = current; 
+	            current = next; 
+	        } 
+	        node = prev; 
+	        return node; 
+	    } 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
