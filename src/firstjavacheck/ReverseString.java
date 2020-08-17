5689a51b4f07dc214a1eb21a956e716b798fd5e3
lakshmi seshu kalvakuri <42748738+seshukalvakuri@users.noreply.github.com>
2020-08-17 10:42:53 -0600

    stringreerse

---
+++ src/firstjavacheck/ReverseString.java
@@ -0,0 +1,25 @@
+package firstjavacheck;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = "lakshmi"; 
+		  
+        // getBytes() method to convert string  
+        // into bytes[]. 
+        byte [] strAsByteArray = input.getBytes(); 
+  
+        byte [] result =  
+                   new byte [strAsByteArray.length]; 
+  
+        // Store result in reverse order into the 
+        // result byte[] 
+        for (int i = 0; i<strAsByteArray.length; i++) 
+            result[i] =  
+             strAsByteArray[strAsByteArray.length-i-1]; 
+  
+        System.out.println(new String(result)); 
+	}
+
+}
